604b9daf1d6968a7b641d57a8cbf8fe82378e1a8
David <softw.dev.dave@gmail.com>
2021-03-04 14:24:56 +0100

    tags added

--- movies/views.py
+++ movies/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, Category
 from .forms import ReviewForm
-
-# class MoviesView(View):
-#     def get(self, request):
-#         movies = Movie.objects.all()
-#         return render(request, "movies/movies.html", {"movie_list": movies})
 
 
 class MoviesView(ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = "movies/movies.html"
-
-
-# class MovieDetailView(View):
-#     def get(self, request, slug):
-#         movie = Movie.objects.get(url=slug)
-#         return render(request, "movies/moviesingle.html", {"movie": movie})
 
 
 class MovieDetailView(DetailView):
